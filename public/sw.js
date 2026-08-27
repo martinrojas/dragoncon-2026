a733a566cee3f8d544835938576ac5149eab533b
Martin Rojas <317579+martinrojas@users.noreply.github.com>
2026-08-26 20:29:25 -0400

    chore: bump PWA service worker cache to v2

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dragoncon-pwa-v1";
+const CACHE_NAME = "dragoncon-pwa-v2";
 const ASSETS_TO_CACHE = ["/", "/manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
